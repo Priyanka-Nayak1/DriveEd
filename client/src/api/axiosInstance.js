003c27adbdafc020c5d21a17fde66224ddd488c7
vivekevdt <vivek.evdt@gmail.com>
2025-06-22 22:44:29 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://driveedapi.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
